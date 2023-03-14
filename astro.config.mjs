e8981582065f95f021218dfe42eb098907d2c2fb
Rodrigo Soares <rodrigosoares.dev@gmail.com>
2023-03-14 13:48:37 -0300

    Google tag manager added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
+import partytown from "@astrojs/partytown";
+
 const site = import.meta.env.BASE_URL;
-
-// https://astro.build/config
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibw.com.br' || site,
-  integrations: [image()],
+  integrations: [image(), partytown()],
   output: "server",
   adapter: netlify()
 });
